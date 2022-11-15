479182b474d7994f1f4081368b44a13b99ca4ca1
Henrique-Schneider <domeneider@gmail.com>
2022-11-15 20:55:10 -0300

    atualizando

--- CheckpointI/Main.java
+++ CheckpointI/Main.java
@@ -1,10 +1,19 @@
 package CheckpointI;
 
+import CheckPoint_teste.Musica;
+
 public class Main {
     public static void main(String[] args) {
-        Musica musica1 = new Musica(1001,"Love Never Felt So Good","POP","Michael Jackson","",4.90);
 
+        Musica[] musicas = new Musica[2];
+        //Musica musica1 = new Musica(1001,"Love Never Felt So Good","POP","Michael Jackson","",4.90);
+        musicas[0]=new Musica(1,"Mundo Bita2","Infantil2","Bita2",80L,4.00);
+        musicas[1] = new Musica(2,"Roda-Roda","Punk","zaza",299L,2.00);
        // System.out.println("Id: " + musica1.getIdMusica() +  " nome: " + musica1.getNomeMusica()+ " " + musica1.getGeneroMusica()+ " " + musica1.getCantor()+ " " + musica1.getTempoDuracao()+ " " + musica1.getNota());
+
+        for (Musica m:musicas){
+            m.info();
+        }
     }
 
 
--- CheckpointI/Musica.java
+++ CheckpointI/Musica.java
@@ -1,6 +1,8 @@
 package CheckpointI;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 public class Musica {
 
@@ -8,17 +10,22 @@ public class Musica {
     private String nomeMusica;
     private String generoMusica;
     private String cantor;
-    private String tempoDuracao;
+
+    private Long tempo;
+    private Duration tempoDuracao;
     private Double nota;
 
-    public Musica(Integer idMusica, String nomeMusica, String generoMusica, String cantor, String tempoDuracao, Double nota) {
+    public Musica(Integer idMusica, String nomeMusica, String generoMusica, String cantor,Long tempo ,Duration tempoDuracao, Double nota) {
         this.idMusica = idMusica;
         this.nomeMusica = nomeMusica;
         this.generoMusica = generoMusica;
         this.cantor = cantor;
+        this.tempo = tempo;
         this.tempoDuracao = tempoDuracao;
         this.nota = nota;
     }
+
+
 
     public void modificarNota(){
 
@@ -56,11 +63,35 @@ public class Musica {
         this.cantor = cantor;
     }
 
-    public String getTempoDuracao() {
-        return tempoDuracao;
+    public Long getTempo() {
+        return tempo;
     }
 
-    public void setTempoDuracao(String tempoDuracao) {
+    public void setTempo(Long tempo) {
+        this.tempo = tempo;
+    }
+
+    public String getTempoDuracao() {
+
+        tempoDuracao = Duration.ofSeconds(getTempo());
+        long dias = tempoDuracao.toDays();
+        Duration d2 = tempoDuracao.minus(dias, ChronoUnit.DAYS);
+        long horas = d2.toHours();
+        Duration d3 = d2.minus(horas, ChronoUnit.HOURS);
+        long minutos = d3.toMinutes();
+        Duration d4 = d3.minus(minutos, ChronoUnit.MINUTES);
+        long segundos = d4.getSeconds();
+        Duration d5 = d4.minus(segundos, ChronoUnit.SECONDS);
+        long nanos = d5.toNanos();
+        Duration d6 = d5.minus(nanos, ChronoUnit.NANOS);
+        if (!d6.isZero()) throw new AssertionError(d6.toString());
+        //        System.out.printf("Duração............:%s%n",horas + " horas, " + minutos + " minutos, " + segundos + " segundos.");
+//        System.out.println("Resultado: " + tempoDuracao);
+
+        return String.format("%dH:%02dm:%02ds",horas,minutos,segundos);
+    }
+
+    public void setTempoDuracao(Duration tempoDuracao) {
         this.tempoDuracao = tempoDuracao;
     }
 
@@ -70,5 +101,15 @@ public class Musica {
 
     public void setNota(Double nota) {
         this.nota = nota;
+    }
+
+    public void info(){
+        System.out.println("____________________________");
+        System.out.printf("Id............:%s%n",this.idMusica);
+        System.out.printf("Nome............:%s%n",this.nomeMusica);
+        System.out.printf("Genero............:%s%n",this.generoMusica);
+        System.out.printf("Cantor............:%s%n",this.cantor);
+        System.out.printf("Duração............:%s%n",getTempoDuracao());
+        System.out.printf("Nota............:%s%n",this.nota);
     }
 }
